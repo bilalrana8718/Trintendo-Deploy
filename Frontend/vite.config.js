074ec7d80810477eb32ab0c2b910d41e8c679726
Rana Bilal Akbar <bilal.rana8718@gmail.com>
2025-04-29 21:51:23 +0500

    Deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,6 +11,10 @@ export default defineConfig({
     },
   },
   server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy" : "require-corp",
+      "Cross-Origin-Opener-Policy" : "same-origin"
+    },
     port: 3000,
     proxy: {
       "/api": {
